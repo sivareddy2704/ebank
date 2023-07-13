edfb17aaf418b867cd8d0226cc27ac96926a1873
sivareddy2704 <sivareddy2704@gmail.com>
2023-07-13 11:23:48 +0000

    ebank commit

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/ebank-not-found-img.png "
+      className="notfound-img"
+      alt="not found"
+    />
+    <h1 className="heading">Page Not Found</h1>
+    <p className="paragraph">
+      We are sorry, the page you requested could not be found
+    </p>
+  </div>
+)
+
+export default NotFound
